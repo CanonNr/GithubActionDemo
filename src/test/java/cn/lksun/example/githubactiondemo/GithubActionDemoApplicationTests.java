d0bf1f963e6adda70afa30bd3aaeb830a4730b1a
Canon <lksun2333@outlook.com>
2022-08-03 16:05:01 +0800

    🚀 first commit

---
+++ src/test/java/cn/lksun/example/githubactiondemo/GithubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lksun.example.githubactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
